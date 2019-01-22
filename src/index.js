0765fa48a7b326d6f57047a14d825baa11b5f7a8
Linda Saraguro <lindasaraguro@yahoo.com>
2019-01-22 16:18:19 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('works');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoibGluZGFzYXJhZ3VybyIsImEiOiJjanI4OTBreWMwNDFhNDNsaDZ2OGJ0aGE3In0.ybegQ1ql9KBynbTJITDmQg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
